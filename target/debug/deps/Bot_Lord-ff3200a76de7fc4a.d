185874b2ab3db88f1926846f7714ee98cd22a345
XVadorZ <eliecharlesdesaulniers@gmail.com>
2020-10-30 16:49:49 -0400

    Idk
    True

--- target/debug/deps/Bot_Lord-ff3200a76de7fc4a.d
+++ target/debug/deps/Bot_Lord-ff3200a76de7fc4a.d
@@ -1,5 +1,5 @@
-c:\Users\eliec\Documents\BotLord\Bot-Lord\target\debug\deps\Bot_Lord-ff3200a76de7fc4a.rmeta: src\main.rs
+c:\Users\eliec\Documents\GitHub\BotLord\target\debug\deps\Bot_Lord-ff3200a76de7fc4a.rmeta: src\main.rs
 
-c:\Users\eliec\Documents\BotLord\Bot-Lord\target\debug\deps\Bot_Lord-ff3200a76de7fc4a.d: src\main.rs
+c:\Users\eliec\Documents\GitHub\BotLord\target\debug\deps\Bot_Lord-ff3200a76de7fc4a.d: src\main.rs
 
 src\main.rs:
